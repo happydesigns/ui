fd9f79eb77da933c0d73491d495ebc027f9948f9
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2024-09-02 23:37:50 +0200

    chore: use @antfu/eslint-config

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import antfu from '@antfu/eslint-config'
+import nuxt from './.playground/.nuxt/eslint.config.mjs'
+
+export default antfu(
+  {},
+  {
+    name: 'tailwind/yaml/rules',
+    files: ['**/*.yaml', '**/*.yml'],
+    rules: {
+      'yaml/plain-scalar': ['off'],
+    },
+  },
+  {
+    name: 'nuxt-studio/md/rules',
+    files: ['**/*.md'],
+    rules: {
+      'no-irregular-whitespace': ['off'],
+    },
+  },
+  nuxt,
+)
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  compatibilityDate: "2024-09-02"
+  modules: [
+    '@nuxt/eslint',
+  ],
+  eslint: {
+    config: {
+      stylistic: true,
+      standalone: false,
+    },
+  },
+  compatibilityDate: '2024-09-02',
 })
