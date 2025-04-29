3c293f79bff8d7a015f2b4e4cbef12f870fd2f64
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2025-04-29 20:03:44 +0200

    feat: update FooterColumns to UI v3

--- app/components/FooterColumns.vue
+++ app/components/FooterColumns.vue
@@ -1,61 +1,93 @@
 <script setup lang="ts" generic="T extends FooterColumnLink">
-import type { FooterColumnLink, FooterColumnsProps, FooterColumnsSlots } from '@nuxt/ui-pro'
-import { tv } from 'tailwind-variants'
-
-// interface FooterColumn {
-//   label: string
-//   children: FooterLink[]
-// }
-
-// const props = defineProps<{
-//   links?: FooterColumn[]
-//   copyright?: string
-//   class?: string
-//   ui?: Partial<typeof config.value>
-// }>()
+import type { FooterColumn, FooterColumnLink, FooterColumnsProps, FooterColumnsSlots } from '@nuxt/ui-pro'
+import { useAppConfig } from '#imports'
 
 interface HFooterColumnProps<T> extends /* @vue-ignore */ FooterColumnsProps<T> {
+  lgCols?: number
+  xlCols?: number
 }
 
 const props = defineProps<HFooterColumnProps<T>>()
 const slots = defineSlots<FooterColumnsSlots<T>>()
-
 const appConfig = useAppConfig()
-const columns = props.columns ?? appConfig?.app?.footer?.links ?? []
 
-const leftColumnCount = slots.left ? slots.left({}).length : 0
-const centerColumnCount = (columns.length ?? 0) as number
-const rightColumnCount = slots.right ? slots.right({}).length : 0
+const footerColumns: FooterColumn<T>[] = props.columns
+  ?? (appConfig?.app?.links?.footer as FooterColumn<T>[] | undefined)
+  ?? []
 
-const lgColumnCount = Math.max(leftColumnCount, centerColumnCount, rightColumnCount)
-const totalColumnCount = leftColumnCount + centerColumnCount + rightColumnCount
+const leftCount = slots.left ? slots.left({}).length : 0
+const centerCount = footerColumns?.length ?? 0
+const rightCount = slots.right ? slots.right({}).length : 0
 
-const footerColumns = tv({
-  slots: appConfig.uiPro?.footerColumns?.slots || {},
-  // extend: props.ui,
-})
+const lgCount = Math.max(leftCount, centerCount, rightCount)
+const totalCount = leftCount + centerCount + rightCount
 
-const ui = footerColumns()
+const ui = {
+  root: 'grid gap-8 grid-flow-row footer-cols-root',
+  left: 'footer-cols-left',
+  center: 'footer-cols-center',
+  right: 'footer-cols-right',
+}
 </script>
 
 <template>
-  <UFooterColumns :columns="appConfig.app?.footer?.links" :ui="{ root: 'xl:grid-cols-2 bg-red-500' }">
-    <template #left>
-      <template v-if="$slots.left">
-        <div class="bg-red-500">
-          {{ lgColumnCount }} {{ totalColumnCount }}
-          <slot name="left" />
-        </div>
-      </template>
+  <UFooterColumns
+    v-bind="props" :columns="footerColumns" :ui
+    :style="{
+      '--cols-sm': 1,
+      '--cols-lg': props.lgCols ?? lgCount,
+      '--cols-xl': props.xlCols ?? totalCount,
+      '--span-left': leftCount,
+      '--span-center': centerCount,
+      '--span-right': rightCount,
+    }"
+  >
+    <template v-if="leftCount > 0" #left>
+      <slot name="left" />
     </template>
-    <template #right>
-      <template v-if="$slots.right">
-        <slot name="right" />
-      </template>
-      <!-- <div :class="ui?.center?.()" class="flex flex-col lg:grid grid-flow-col auto cols-fr gap-8 xl:col-span-2 grid-cols-3 xl:grid-cols-none text-sm">
-        <Snippet path="/snippets/address" :prose="false" />
-        <Snippet path="/snippets/contact" :prose="false" />
-      </div> -->
+    <template v-if="rightCount > 0" #right>
+      <slot name="right" />
     </template>
   </UFooterColumns>
 </template>
+
+<style>
+@import "tailwindcss";
+
+.footer-cols-left {
+  grid-column: span var(--span-left);
+}
+
+.footer-cols-center {
+  grid-column: span var(--span-center);
+}
+
+.footer-cols-right {
+  grid-column: span var(--span-right);
+}
+
+.footer-cols-left,
+.footer-cols-center,
+.footer-cols-right {
+  display: contents;
+}
+.footer-cols-root {
+  grid-template-columns: repeat(var(--cols-sm), minmax(0, 1fr));
+}
+
+@media (min-width: theme(--breakpoint-lg)) {
+  .footer-cols-root {
+    grid-template-columns: repeat(var(--cols-lg), minmax(0, 1fr));
+  }
+}
+
+@media (min-width: theme(--breakpoint-xl)) {
+  .footer-cols-root {
+    grid-template-columns: repeat(var(--cols-xl), minmax(0, 1fr));
+  }
+}
+
+.footer-cols-root p:last-child {
+  margin-bottom: 0;
+}
+</style>
