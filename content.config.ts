705b2bcc23d0838b338ac2bf702582f617f2b571
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2026-03-12 21:48:31 +0100

    refactor: export content schemas separately in content.config.ts

--- content.config.ts
+++ content.config.ts
@@ -5,16 +5,6 @@ export const socialSchema = z.object({
   url: z.string(),
 })
 
-export const userSchema = z.object({
-  name: z.string(),
-  description: z.string().optional(),
-  username: z.string().optional(),
-  to: z.string().optional(),
-  avatar: property(z.object({})).inherit('@nuxt/ui/components/Avatar.vue').optional(),
-  socials: z.array(socialSchema).optional(),
-  email: z.string().email().optional(),
-})
-
 export const snippetCollectionConfig = defineCollection({
   type: 'page',
   source: {
@@ -22,20 +12,29 @@ export const snippetCollectionConfig = defineCollection({
     prefix: '/snippets',
   },
 })
+
+export const articleSchema = z.object({
+  date: z.string().optional(),
+  dateEnd: z.string().optional(),
+  authors: z.array(z.string()).optional(),
+  category: z.string().optional(),
+  toc: z.boolean().default(true),
+  header: property(z.object({})).inherit('@nuxt/ui/components/PageHeader.vue').optional(),
+})
+
 export const articleCollectionConfig = defineCollection({
   type: 'page',
   source: {
     include: 'articles/**/*.{md,yaml}',
     prefix: '/articles',
   },
-  schema: z.object({
-    date: z.string().optional(),
-    dateEnd: z.string().optional(),
-    authors: z.array(z.string()).optional(),
-    category: z.string().optional(),
-    toc: z.boolean().default(true),
-    header: property(z.object({})).inherit('@nuxt/ui/components/PageHeader.vue').optional(),
-  }),
+  schema: articleSchema,
+})
+
+export const pageSchema = z.object({
+  layout: z.enum(['default', 'content']),
+  toc: z.boolean().default(true),
+  header: property(z.object({})).inherit('@nuxt/ui/components/PageHeader.vue').optional(),
 })
 
 export const pageCollectionConfig = defineCollection({
@@ -44,11 +43,17 @@ export const pageCollectionConfig = defineCollection({
     include: 'pages/**/*.{md,yaml}',
     prefix: '/',
   },
-  schema: z.object({
-    layout: z.enum(['default', 'content']),
-    toc: z.boolean().default(true),
-    header: property(z.object({})).inherit('@nuxt/ui/components/PageHeader.vue').optional(),
-  }),
+  schema: pageSchema,
+})
+
+export const userSchema = z.object({
+  name: z.string(),
+  description: z.string().optional(),
+  username: z.string().optional(),
+  to: z.string().optional(),
+  avatar: property(z.object({})).inherit('@nuxt/ui/components/Avatar.vue').optional(),
+  socials: z.array(socialSchema).optional(),
+  email: z.string().email().optional(),
 })
 
 export const userCollectionConfig = defineCollection({
