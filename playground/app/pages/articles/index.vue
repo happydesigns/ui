485c72112ac5fbc839f55b0729cdf0ab282247f7
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2026-03-17 20:22:15 +0100

    style(playground): improve articles index layout and cleanup configuration

--- playground/app/pages/articles/index.vue
+++ playground/app/pages/articles/index.vue
@@ -9,13 +9,31 @@ const header = computed(() => resolvePageHeader(page.value))
 
 <template>
   <NuxtLayout>
-    <UContainer v-if="page" class="py-12">
-      <UPageHeader
-        v-if="header"
-        v-bind="(header as any)"
-        class="mb-12"
-      />
+    <UPageHero
+      v-if="header"
+      v-bind="(header as any)"
+      orientation="horizontal"
+    >
+      <template #description>
+        {{ header.description }}
 
+        <div class="mt-4">
+          <UButton
+            to="/articles/rss.xml"
+            color="neutral"
+            external
+            icon="i-lucide-rss"
+            variant="subtle"
+            size="xs"
+            target="_blank"
+          >
+            RSS
+          </UButton>
+        </div>
+      </template>
+    </UPageHero>
+
+    <UContainer v-if="page">
       <UPageBody>
         <HArticleList />
       </UPageBody>
