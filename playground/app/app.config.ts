548f226a37b059974d5f6c85f1520817820993db
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2026-03-17 20:25:40 +0100

    style(playground): configuration cleanup and UI overrides

--- playground/app/app.config.ts
+++ playground/app/app.config.ts
@@ -1,4 +1,4 @@
-import type { ButtonProps } from '#ui/types'
+import type { ButtonProps } from '@nuxt/ui'
 
 export default defineAppConfig({
   app: {
@@ -130,6 +130,23 @@ export default defineAppConfig({
     },
     footerColumns: {
       slots: {
+      },
+    },
+    pageHero: {
+      slots: {
+        container: 'py-10 sm:py-20 lg:py-20',
+        title: 'sm:text-5xl',
+      },
+    },
+    prose: {
+      img: {
+        base: 'w-full',
+      },
+      codeTree: {
+        slots: {
+          root: 'bg-default m-0',
+          content: '[&>div>pre]:rounded-r-none',
+        },
       },
     },
   },
