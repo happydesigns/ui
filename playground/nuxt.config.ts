4fb578f0ade40cfdb2e7664cacc88a8d4c0eac6f
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2026-03-23 09:03:43 +0100

    chore(playground): add studio repo

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,3 +1,12 @@
 export default defineNuxtConfig({
   extends: ['..'],
+  studio: {
+    repository: {
+      provider: 'github',
+      owner: 'happydesigns',
+      repo: 'ui',
+      branch: 'main',
+      rootDir: 'playground'
+    },
+  },
 })
